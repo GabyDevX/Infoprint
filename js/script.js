10f86b0574fbeb46f5ed0958dd3eea772681db86
Gianluca <gianlucafava.17@gmail.com>
2022-08-17 21:03:42 -0300

    incorpore libreria Email

--- js/script.js
+++ js/script.js
@@ -42,3 +42,23 @@ mostrarModalImpresionD();
 mostrarModalImpresionO();
 mostrarModalMerch();
 
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+    .addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        btn.value = 'Enviando...';
+
+        const serviceID = 'default_service';
+        const templateID = 'template_l8rmjh9';
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.value = 'Enviar';
+                alert('Enviado correctamente!');
+            }, (err) => {
+                btn.value = 'Enviar';
+                alert(JSON.stringify(err));
+            });
+    });
