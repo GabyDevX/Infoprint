46a7437d5f3754b18b9624752773f8da66fc1406
GabyDevX <gabo.ramirez0811@gmail.com>
2022-08-18 12:18:35 -0300

    Se implementó sweet alert para tener una interacción con el usuario más amigable a la hora de contactar mediante el formulario

--- js/script.js
+++ js/script.js
@@ -1,64 +1,68 @@
-let botonImpresionD = document.getElementById("boton-impresionD");
-let botonImpresionO = document.getElementById("boton-impresionO");
-let botonMerch = document.getElementById("boton-merch");
+let botonImpresionD = document.getElementById('boton-impresionD')
+let botonImpresionO = document.getElementById('boton-impresionO')
+let botonMerch = document.getElementById('boton-merch')
 
 //FUNCIÓN PARA MOSTRAR Y CERRAR EL MODAL DE PRODUCTOS
 const mostrarModalImpresionD = () => {
-    let modal_container = document.getElementById("modal_container-impresionD");
-    botonImpresionD.addEventListener("click", () => {
-        modal_container.classList.add("show");
-    });
-    const cerrar = document.getElementById("close-impresionD");
-    cerrar.addEventListener("click", () => {
-        modal_container.classList.remove("show");
-    });
-};
-
+  let modal_container = document.getElementById('modal_container-impresionD')
+  botonImpresionD.addEventListener('click', () => {
+    modal_container.classList.add('show')
+  })
+  const cerrar = document.getElementById('close-impresionD')
+  cerrar.addEventListener('click', () => {
+    modal_container.classList.remove('show')
+  })
+}
 
 const mostrarModalImpresionO = () => {
-    let modal_container = document.getElementById("modal_container-impresionO");
-    botonImpresionO.addEventListener("click", () => {
-        modal_container.classList.add("show");
-    });
-    const cerrar = document.getElementById("close-impresionO");
-    cerrar.addEventListener("click", () => {
-        modal_container.classList.remove("show");
-    });
-};
-
+  let modal_container = document.getElementById('modal_container-impresionO')
+  botonImpresionO.addEventListener('click', () => {
+    modal_container.classList.add('show')
+  })
+  const cerrar = document.getElementById('close-impresionO')
+  cerrar.addEventListener('click', () => {
+    modal_container.classList.remove('show')
+  })
+}
 
 const mostrarModalMerch = () => {
-    let modal_container = document.getElementById("modal_container-merch");
-    botonMerch.addEventListener("click", () => {
-        modal_container.classList.add("show");
-    });
-    const cerrar = document.getElementById("close-merch");
-    cerrar.addEventListener("click", () => {
-        modal_container.classList.remove("show");
-    });
-};
+  let modal_container = document.getElementById('modal_container-merch')
+  botonMerch.addEventListener('click', () => {
+    modal_container.classList.add('show')
+  })
+  const cerrar = document.getElementById('close-merch')
+  cerrar.addEventListener('click', () => {
+    modal_container.classList.remove('show')
+  })
+}
 
-mostrarModalImpresionD();
-mostrarModalImpresionO();
-mostrarModalMerch();
+mostrarModalImpresionD()
+mostrarModalImpresionO()
+mostrarModalMerch()
 
-const btn = document.getElementById('button');
+const btn = document.getElementById('button')
 
-document.getElementById('form')
-    .addEventListener('submit', function (event) {
-        event.preventDefault();
+document.getElementById('form').addEventListener('submit', function (event) {
+  event.preventDefault()
 
-        btn.value = 'Enviando...';
+  btn.value = 'Enviando...'
 
-        const serviceID = 'default_service';
-        const templateID = 'template_l8rmjh9';
+  const serviceID = 'default_service'
+  const templateID = 'template_l8rmjh9'
 
-        emailjs.sendForm(serviceID, templateID, this)
-            .then(() => {
-                btn.value = 'Enviar';
-                alert('Enviado correctamente!');
-            }, (err) => {
-                btn.value = 'Enviar';
-                alert(JSON.stringify(err));
-            });
-    });
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = 'Enviar'
+      Swal.fire({
+        title: 'Exito!',
+        text: 'Pronto nos pondremos en contacto contigo.',
+        icon: 'success',
+        confirmButtonText: 'Continuar',
+      })
+    },
+    (err) => {
+      btn.value = 'Enviar'
+      alert(JSON.stringify(err))
+    },
+  )
+})
